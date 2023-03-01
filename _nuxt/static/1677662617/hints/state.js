61f40a50aee2278ae17a9d212bc31ba314fa46ee
sin <shulgin.grigoriy@yandex.ru>
2023-03-01 12:25:50 +0300

    Updates

---
+++ _nuxt/static/1677662617/hints/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"gigdata\u002F_nuxt\u002Fstatic\u002F1677662617",layout:"default",error:a,state:{windowWidth:0},serverRendered:true,routePath:"\u002Fhints",config:{_app:{basePath:"gigdata\u002F",assetsPath:"gigdata\u002F_nuxt\u002F",cdnURL:a}}}}(null));
