57f8f3c76594a8bca9de0daea78ba6d5f5a7d91b
sin <shulgin.grigoriy@yandex.ru>
2023-03-03 11:56:21 +0300

    Updates

--- _nuxt/static/1677757779/API/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgigdata\u002F_nuxt\u002Fstatic\u002F1677757779",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{windowWidth:0},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fgigdata\u002F",assetsPath:"\u002Fgigdata\u002F_nuxt\u002F",cdnURL:null}}}}("\u002FAPI"));
