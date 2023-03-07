17215aad0ba53d261785bb66004ad11f1c380469
sin <shulgin.grigoriy@yandex.ru>
2023-03-07 21:48:42 +0300

    Updates

--- _nuxt/static/1678199924/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fhints\u002FhowToConnect","\u002Fhints","\u002Fhints\u002FhowToConnect\u002Fhints-address"]})
---
+++ _nuxt/static/1678214597/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fhints\u002FhowToConnect","\u002Fhints\u002FhowToConnect\u002Fhints-address","\u002Fhints"]})
